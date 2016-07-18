eac10d8e6c4a77b28ba31abe617364e6009d0b0c
E. R. C. Pereda <ercp42@gmail.com>
2016-07-18 04:07:23 -0300

    test(index): Add unit test for the library
    closes #2

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,7 +2,26 @@ const expect = require('chai').expect;
 const lib = require('./index');
 
 describe('rp1-characters', () => {
-  it('should work', () => {
-    expect(true).to.be.true;
+  describe('all', () => {
+    it('should be an array of characters', () => {
+      expect(lib.all).to.satisfy(isArrayOfCharacters);
+
+      function isArrayOfCharacters(array) {
+        return array.every((item) => {
+          return typeof item === 'object' && item.name !== undefined && item.avatar !== undefined;
+        });
+      }
+    });
+
+    it('should contain Parzival', () => {
+      expect(lib.all).to.include({name: "Wade Owen Watts", avatar: "Parzival"});
+    });
+  }); 
+
+  describe('random', () => {
+    it('should return a random item from lib.all', () => {
+      const randomItem = lib.random();
+      expect(lib.all).to.include(randomItem);
+    });
   });
 });
